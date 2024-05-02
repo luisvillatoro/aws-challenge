e25d1b49e22ba1c99c5e001d553180f3703680b8
luisvillatoro <lafvillatoro@gmail.com>
2024-05-01 21:47:54 -0600

    testing code change

--- api_publico.py
+++ api_publico.py
@@ -4,6 +4,6 @@ import json
 def lambda_handler(event, context):
     response = {
         "statusCode": 200,
-        "body": json.dumps("¡Hola desde AWS Lambda! Mi nombre es..."),
+        "body": json.dumps("Hola desde AWS Lambda! Mi nombre es"),
     }
     return response
